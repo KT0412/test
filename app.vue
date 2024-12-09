d752ad015c901f32fa97c5cc63da060526260cd6
竹内桂 <keitake1207@gmail.com>
2024-12-09 10:48:26 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+
+<script setup>
+ import { supabase } from './utils/spabase.ts'
+
+ let { data: tests, error } = await supabase.from('tests').select('*');
+          
+</script>
+
+<template>
+  <ul>
+    <li v-for="test in tests" :key="test.id">{{ test.title }}</li>
+  </ul>
+</template>
+<script>
+
+
+</script>
+
+
